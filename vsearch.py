b01f8d3aed9f86c5ea15a51aef4fdbd1af841ff5
sp-moribito <79080501+sp-moribito@users.noreply.github.com>
2021-03-03 17:33:39 +0900

    Add files via upload

--- vsearch.py
+++ vsearch.py
@@ -1,6 +1,21 @@
-def search4vowels(word):
+def search4vowels(word:str) -> set:
     '''Return a boolean based on any vowels found.'''
     vowels=set('aeiou')
     return vowels.intersection(set(word))
 
+search4vowels('hitch-hiker')
+
+search4vowels('galaxy')
+
+search4vowels('life, the universe and everything')
+
 search4vowels('sky')
+
+l = list()
+l
+
+d=dict()
+d
+
+t=tuple()
+t
---
+++ vsearch1.py
@@ -0,0 +1,16 @@
+def search4vowels(phrase:str) -> set:
+    '''Return any vowels found in a supplied phrase.'''
+    vowels=set('aeiou')
+    return vowels.intersection(set(phrase))
+
+def search4letters(phrase:str,letters:str) -> set:
+    '''Return a set based on any 'letters' found in 'phrase'.'''
+    return set(letters).intersection(set(phrase))
+
+search4letters('hitch-hiker','aeiou')
+
+search4letters('galaxy','xyz')
+
+search4letters('life, the universe and everything','o')
+
+search4letters('sky')
