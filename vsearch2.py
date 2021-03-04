ce16fe1dc24a8a2bae390dbb24b09edc5a96a2fe
sp-moribito <79080501+sp-moribito@users.noreply.github.com>
2021-03-04 16:52:33 +0900

    Add files via upload

--- vsearch2.py
+++ vsearch2.py
@@ -2,12 +2,12 @@ def search4letters(phrase:str,letters:str='aeiou') -> set:
     '''Return a set based on any 'letters' found in 'phrase'.'''
     return set(letters).intersection(set(phrase))
 
-search4letters('hitch-hiker','aeiou')
+#search4letters('hitch-hiker','aeiou')
 
-search4letters(letters='galaxy',phrase='xyz')
+#search4letters(letters='galaxy',phrase='xyz')
 
-search4letters('life, the universe and everything','o')
+#search4letters('life, the universe and everything','o')
 
 
-import random
-random.randint(0,255)
+#import random
+#random.randint(0,255)
